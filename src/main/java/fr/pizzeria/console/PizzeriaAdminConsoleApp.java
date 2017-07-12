c4fffb69be6c8cbb4304edaeaa73233516ee72a2
kuntzthomas <thomas.kuntz68@gmail.com>
2017-07-12 14:27:44 +0200

    Update PizzeriaAdminConsoleApp.java

--- src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -11,7 +11,7 @@ public class PizzeriaAdminConsoleApp {
 
 	public static void main(String[] args) throws Exception{
 		
-		Menu menu = new Menu()
+		Menu menu = new Menu();
 		
 		menu.manage();
 		
